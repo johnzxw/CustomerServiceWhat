e9c81ee3a1943dd13543c318a7230a37e1d0296b
john zhouxw <john.zhouxw@gmail.com>
2018-03-30 19:30:57 +0800

    add log record

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 npm install -g socket.io
-npm install -g hiredis redis
+npm install -g mongoose
 npm install -g supervisor
--- js/app.js
+++ js/app.js
@@ -6,6 +6,38 @@ var io = require('socket.io')(app);
 var PORT = 8081;
 /*定义用户数组*/
 var users = [];
+
+const mongoose = require("mongoose");
+/* 连接本地mongodb */
+mongoose.connect("mongodb://127.0.0.1:27017/chat", function (err) {
+    if (!err) {
+        console.log("connected to Mongodb");
+    } else {
+        throw err;
+    }
+});
+const Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+var ChatlogSchema = new Schema({
+    data: {type: String},
+    sendUser: {type: String},
+    receiveUser: {type: String},
+    roomId: {type: String},
+    receiveTime: {type: Number, index: true},
+    date: {type: Date, default: Date.now}
+
+});
+var ChatUserLoginSchema = new Schema({
+    name: {type: String},
+    ip: {type: String},
+    roomId: {type: String},
+    loginTime: {type: Number, index: true},
+    date: {type: Date, default: Date.now}
+
+});
+//新建model 设置表名称为chatlog
+var ChatlogModel = mongoose.model("chatlog", ChatlogSchema, "chatlog");
+var ChatUserLoginModel = mongoose.model("chatuserloginlog", ChatUserLoginSchema, "chatuserloginlog");
 app.listen(PORT, {origins: '*:*'});
 io.on('connection', function (socket) {
     /*是否是新用户标识*/
@@ -39,6 +71,17 @@ io.on('connection', function (socket) {
             if (roomId) {
                 socket.join(roomId);
             }
+            var ChatUserLogin = new ChatUserLoginModel();
+            ChatUserLogin.name = data.username;
+            ChatUserLogin.roomId = roomId;
+            ChatUserLogin.ip = socket.handshake.address;
+            ChatUserLogin.loginTime = new Date().getTime();
+            ChatUserLogin.save(function (err) {
+                if (err) {
+                    console.log('save failed');
+                }
+                console.log("save success");
+            });
             socket.emit('loginSuccess', returnData);
             /*向所有连接的客户端广播add事件*/
             io.sockets.to(roomId).emit('add', data);
@@ -51,6 +94,18 @@ io.on('connection', function (socket) {
     /*监听发送消息*/
     socket.on('sendMessage', function (data) {
         // socket.emit('receiveMessage', data);
+        var LogInsert = new ChatlogModel();
+        LogInsert.data = data.message;
+        LogInsert.roomId = roomId;
+        LogInsert.sendUser = data.username;
+        LogInsert.receiveUser = "";
+        LogInsert.receiveTime = new Date().getTime();
+        LogInsert.save(function (err) {
+            if (err) {
+                console.log('save failed');
+            }
+            console.log("save success");
+        });
         io.sockets.to(roomId).emit('receiveMessage', data);
     });
 
